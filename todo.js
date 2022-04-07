4712863fa32de1113197deedf4cfdb0abeba8088
Kojiro Asano <45510175+KojiroAsano@users.noreply.github.com>
2022-04-06 17:02:41 -0700

    Add due date

--- todo.js
+++ todo.js
@@ -1,12 +1,12 @@
 function Todo({todo, index, remove}){
     function handle(){
-        if (!window.confirm(`Are you remove "${todo.text}" from Todo list?`)) {
+        if (!window.confirm(`Are you remove "${todo.text}" by ${todo.due} from Todo list?`)) {
             return;
           }
         remove(index);
     }
-    return <div className="todo">{todo.text}<button type="button" class="btn btn-outline-success" onClick={handle}>
-    Done
-    </button></div>;
+    return <div className="todo">
+        <div className="text">{todo.text}</div>  
+        <div className="due">Due: {todo.due}</div>
+        <button type="button" className="btn btn-outline-success" onClick={handle}>Done </button></div>;
 }
-
